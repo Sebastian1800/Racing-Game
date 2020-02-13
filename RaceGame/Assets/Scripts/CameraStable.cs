7119e15624caf90b8bb539729a09c226fc020e8a
Sebastian <58812929+Sebastian@users.noreply.github.com>
2020-02-13 12:16:23 +0200

    Mapa+masina+timer

---
+++ RaceGame/Assets/Scripts/CameraStable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraStable : MonoBehaviour
+{
+
+	public GameObject TheCar;
+	public float CarX;
+	public float CarY;
+	public float CarZ;
+
+	void Update()
+	{
+		CarX = TheCar.transform.eulerAngles.x;
+		CarY = TheCar.transform.eulerAngles.y;
+		CarZ = TheCar.transform.eulerAngles.z;
+
+		transform.eulerAngles = new Vector3(CarX - CarX, CarY, CarZ - CarZ);// Carx si Carz se foloseste pt a intoarce camera,pt y nu s a folosit pt avem nev sa mearga in sus si in jos 
+
+	}
+}
