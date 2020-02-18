344044288f47ab52563aa81baf6afe2de2239033
Andrei-Sebastian <Andrei-Sebastian@users.noreply.github.com>
2020-02-18 23:06:46 +0200

    concurent
    dsa

---
+++ RaceGame/Assets/Scripts/CarControlActive.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarControlActive : MonoBehaviour
+{
+    GameObject CarControl;
+
+    void Start()
+    {
+       // CarControl.SetActive(true);
+
+    }
+}
+
